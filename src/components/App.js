0a1999ff72df85efa5286c055211da9f06d96185
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-01-26 10:32:36 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,74 @@ import React from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
+import { useState } from "react";
 
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import { CATEGORIES, Tasks } from "../data";
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const mTasks = Tasks.map( (task, index) => {
+    return {...task, id: index}
+  })
+
+  console.log(Tasks)
+  console.log(mTasks)
+
+  const [category, setCategory] = useState("All")
+  const [tasks, setTasks] = useState(mTasks);
+
+  function addTask(newTasks) {
+  function onTaskFormSubmit(newTask) {
+    setTasks([{...newTask, id: tasks.length},...tasks]);
+  }
+
+  function deleteTask(taskId) {
+      let newTasks = tasks.filter(task => {
+        if(task.id === taskId) {
+          return false;
+        }
+        return true;
+      })
+
+      setTasks(newTasks)
+    // alert(taskId)
+    // console.log(tasks)
+    let newTasks = tasks.filter(task => {
+      if(task.id === taskId) {
+        return false
+      } else {
+        return true
+      }      
+    })
+
+    setTasks(newTasks)
+
+      // let newTasks = tasks.filter(task => {
+      //   if(task.id === taskId) {
+      //     return false;
+      //   }
+      //   return true;
+      // })
+
+      // setTasks(newTasks)
+  }
+
+  const tasksToDisplay = tasks.filter(task => {
+    if(category === "All") { 
+      return true;
+    } else {
+      return task.category === category
+    }
+  });
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter updateCategory={setCategory} categories={CATEGORIES} />
+      <NewTaskForm onTaskFormSubmit={addTask} categories={CATEGORIES}/>
+      <NewTaskForm onTaskFormSubmit={onTaskFormSubmit} categories={CATEGORIES}/>
+      <TaskList deleteTask={deleteTask} tasks={tasksToDisplay} />
     </div>
-  );
+  )};
 }
-
 export default App;
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,54 @@
 import React from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, onTaskFormSubmit}) {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //
+  // function onTaskFormSubmit(e) {
+  //   e.preventDefault();
+  //   //
+
+    onTaskFormSubmit({
+      text: e.target.text.value,
+      category: e.target.category.value
+    });
+  //   alert(e.target.text.value)
+
+    e.target.reset()
+  }
+  //   // onTaskFormSubmit({
+  //   //   text: e.target.text.value,
+  //   //   category: e.target.category.value
+  //   // });
+
+  //   // e.target.reset()
+  //}
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit} >
+    <form className="new-task-form" onSubmit={(e) => {
+      onTaskFormSubmit({
+        text: document.getElementById("text").value,
+        category: document.getElementById("category").value
+      });
+    }} >
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" required/>
+        <input id="text" type="text" name="details" required/>
       </label>
       <label>
         Category
         <select name="category">
-          {/* render <option> elements for each category here */}
+        <select id="category" name="category">
+          {
+            categories.map(category=>Boolean(category==='All')? null : <option key={category}>{category}</option>)
+          }
         </select>
       </label>
       <input type="submit" value="Add task" />
     </form>
   );
 }
-
-export default NewTaskForm;
+export default NewTaskForm
